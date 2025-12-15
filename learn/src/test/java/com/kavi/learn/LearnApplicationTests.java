ec4bf0defd2ff79c2ca2970d93ecc8ac606801c3
neojarvis <techadmin@iamneo.ai>
2025-12-15 05:21:25 +0000

    Auto-save-commit-12/15/2025--10:51:25-AM

---
+++ learn/src/test/java/com/kavi/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavi.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
