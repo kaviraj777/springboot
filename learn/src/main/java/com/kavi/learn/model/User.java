6bebe67ea3e6cb98d1d8facda15fcf97cba34c47
neojarvis <techadmin@iamneo.ai>
2025-12-15 06:07:24 +0000

    Auto-save-commit-12/15/2025--11:37:24-AM

--- learn/src/main/java/com/kavi/learn/model/User.java
+++ learn/src/main/java/com/kavi/learn/model/User.java
@@ -1,5 +1,6 @@
 package com.kavi.learn.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -10,9 +11,13 @@ import jakarta.persistence.Table;
 public class User {
     @Id
     private int id;
+    @Column(name="user_name")
     private String name;
+    @Column(name="user_email")
     private String email;
+    @Column(name="user_password")
     private String password;
+    @Column(name="user_role")
     private String role;
     
     public User(){
